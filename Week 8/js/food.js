6231032abc573ff16e09f35d33a0b57789a43531
Mark Harding <markharding6711@gmail.com>
2026-03-09 20:12:40 -0600

    update

--- Week 8/js/food.js
+++ Week 8/js/food.js
@@ -1,50 +1,41 @@
 class Food {
-  constructor(type, x, y) {
+
+  constructor(type) {
 
     this.type = type;
-    this.x = x;
-    this.y = y;
-
     this.size = 50;
-    this.active = true;
 
     if (type === "apple") {
       this.image = appleImg;
-      this.points = -20; // bad food
     }
 
     if (type === "banana") {
       this.image = bananaImg;
-      this.points = 200;
     }
 
     if (type === "cherry") {
       this.image = cherryImg;
-      this.points = 300;
     }
 
     if (type === "strawberry") {
       this.image = strawberryImg;
-      this.points = 400;
     }
+
+    this.relocate();
   }
 
   display() {
-    if (!this.active) return;
     image(this.image, this.x, this.y, this.size, this.size);
   }
 
-  collect() {
-    this.active = false;
-  }
-
-  respawn() {
+  relocate() {
     this.x = random(50, width - 50);
-    this.y = random(-600, -50);
-    this.active = true;
+    this.y = random(-600, -100);
   }
 
-  collides(px, py, psize) {
-    return dist(px, py, this.x, this.y) < this.size/2 + psize/2;
+  collide() {
+    return dist(playerX, playerY, this.x, this.y) <
+      this.size/2 + playerSize/2;
   }
+
 }
--- Week 8/js/sketch.js
+++ Week 8/js/sketch.js
@@ -1,10 +1,4 @@
 // ======================================================
-// GAME STATE
-// ======================================================
-
-let gameState = "start";
-
-// ======================================================
 // PLAYER ANIMATION
 // ======================================================
 
@@ -21,6 +15,7 @@ let animationSpeed = 6;
 let playerState = "idle";
 let playerDirection = 1;
 
+
 // ======================================================
 // PLAYER
 // ======================================================
@@ -34,19 +29,36 @@ let jumpForce = -18;
 
 let playerHealth = 100;
 
+
+// ======================================================
+// DAMAGE + KNOCKBACK
+// ======================================================
+
+let invincible = false;
+let invincibleTimer = 0;
+
+let knockbackX = 12;
+let knockbackY = -10;
+
+
 // ======================================================
 // GAME OBJECTS
 // ======================================================
 
 let platforms = [];
 let enemies = [];
-let foods = [];
+let fruits = [];
+
 
 // ======================================================
 // SCORE
 // ======================================================
 
 let score = 0;
+
+let gameOver = false;
+let gameStarted = false;
+
 
 // ======================================================
 // IMAGES
@@ -60,6 +72,7 @@ let enemy1 = [];
 let enemy2 = [];
 let enemy3 = [];
 
+
 // ======================================================
 // SOUND
 // ======================================================
@@ -69,6 +82,7 @@ let goodFoodSound;
 let badFoodSound;
 let deathSound;
 let backgroundMusic;
+
 
 // ======================================================
 // PRELOAD
@@ -80,34 +94,28 @@ backgroundImg = loadImage("images/backrounds/backround.png");
 
 idleFrame = loadImage("images/player/idle/dude.png");
 
-for(let i=1;i<=8;i++){
+for(let i=1;i<=8;i++)
 jumpFrames.push(loadImage("images/player/jump/dude_jump"+i+".png"));
-}
 
-for(let i=1;i<=4;i++){
+for(let i=1;i<=4;i++)
 hurtFrames.push(loadImage("images/player/hurt/dude_hurt"+i+".png"));
-}
 
-for(let i=1;i<=8;i++){
+for(let i=1;i<=8;i++)
 deathFrames.push(loadImage("images/player/death/dude_death"+i+".png"));
-}
 
 appleImg = loadImage("images/food/apple.png");
 bananaImg = loadImage("images/food/banana.png");
 cherryImg = loadImage("images/food/cherry.png");
 strawberryImg = loadImage("images/food/strawberry.png");
 
-for(let i=1;i<=2;i++){
+for(let i=1;i<=2;i++)
 enemy1.push(loadImage("images/enemies/enemy1_"+i+".png"));
-}
 
-for(let i=1;i<=2;i++){
+for(let i=1;i<=2;i++)
 enemy2.push(loadImage("images/enemies/enemy2_"+i+".png"));
-}
 
-for(let i=1;i<=2;i++){
+for(let i=1;i<=2;i++)
 enemy3.push(loadImage("images/enemies/enemy3_"+i+".png"));
-}
 
 jumpSound = loadSound("sound/jump.mp3");
 goodFoodSound = loadSound("sound/good_item.mp3");
@@ -116,6 +124,7 @@ deathSound = loadSound("sound/death.mp3");
 backgroundMusic = loadSound("sound/backround.mp3");
 
 }
+
 
 // ======================================================
 // SETUP
@@ -128,6 +137,7 @@ imageMode(CENTER);
 
 }
 
+
 // ======================================================
 // START GAME
 // ======================================================
@@ -135,40 +145,36 @@ imageMode(CENTER);
 function startGame(){
 
 playerX = width/2;
-playerY = height - 150;
+playerY = height-150;
 
 velocityY = 0;
 
 score = 0;
 playerHealth = 100;
 
-platforms = [];
-enemies = [];
-foods = [];
+platforms=[];
+enemies=[];
+fruits=[];
 
-// base platform
 platforms.push(new Platform(width/2-60,height-80,120,20));
 
-// generate platforms
 for(let i=0;i<8;i++){
-
 let y = height - i*80;
-
-let type = random(["normal","normal","moving","breakable"]);
-
-platforms.push(
-new Platform(random(width-120),y,120,20,type)
-);
-
+platforms.push(new Platform(random(width-120),y,120,20));
 }
 
-// spawn food
-spawnFood();
+spawnFruits();
 
 playerState="idle";
 currentFrame=0;
 
+gameOver=false;
+gameStarted=true;
+
+backgroundMusic.loop();
+
 }
+
 
 // ======================================================
 // DRAW
@@ -178,12 +184,23 @@ function draw(){
 
 image(backgroundImg,width/2,height/2,width,height);
 
-if(gameState==="start"){
-drawStartScreen();
+if(!gameStarted){
+
+fill(0,180);
+rect(0,0,width,height);
+
+fill(255);
+textAlign(CENTER);
+textSize(40);
+text("DOODLE CLIMB",width/2,200);
+
+textSize(22);
+text("Press SPACE to Start",width/2,300);
+
 return;
 }
 
-if(gameState==="gameover"){
+if(gameOver){
 showGameOver();
 return;
 }
@@ -191,34 +208,17 @@ return;
 handlePlayerMovement();
 handlePlatforms();
 handleEnemies();
-handleFood();
+handleFruits();
 
 updatePlayerAnimation();
 drawUI();
 
+if(invincible && millis()-invincibleTimer>700){
+invincible=false;
 }
 
-// ======================================================
-// START SCREEN
-// ======================================================
-
-function drawStartScreen(){
-
-rectMode(CORNER);
-
-fill(0,180);
-rect(0,0,width,height);
-
-fill(255);
-textAlign(CENTER);
-
-textSize(50);
-text("Sky Climber",width/2,200);
-
-textSize(22);
-text("Press SPACE to Start",width/2,300);
-
 }
+
 
 // ======================================================
 // PLAYER MOVEMENT
@@ -227,38 +227,36 @@ text("Press SPACE to Start",width/2,300);
 function handlePlayerMovement(){
 
 if(keyIsDown(LEFT_ARROW)){
-playerX -= 5;
-playerDirection = -1;
+playerX-=5;
+playerDirection=-1;
 }
 
 if(keyIsDown(RIGHT_ARROW)){
-playerX += 5;
-playerDirection = 1;
+playerX+=5;
+playerDirection=1;
 }
 
-if(playerX < -playerSize/2) playerX = width + playerSize/2;
-if(playerX > width + playerSize/2) playerX = -playerSize/2;
+if(playerX<-playerSize/2) playerX=width+playerSize/2;
+if(playerX>width+playerSize/2) playerX=-playerSize/2;
 
-velocityY += gravity;
-playerY += velocityY;
+velocityY+=gravity;
+playerY+=velocityY;
 
-// scrolling camera
-if(playerY < height/2){
+if(playerY<height/2){
 
-let diff = height/2 - playerY;
-playerY = height/2;
+let diff = height/2-playerY;
+playerY=height/2;
 
-score += diff;
+score+=diff;
 
-for(let p of platforms) p.y += diff;
-for(let e of enemies) e.y += diff;
-for(let f of foods) f.y += diff;
+for(let p of platforms) p.y+=diff;
+for(let e of enemies) e.y+=diff;
+for(let f of fruits) f.y+=diff;
 
 }
 
-if(playerY - playerSize/2 > height){
+if(playerY-playerSize/2>height)
 triggerDeath();
-}
 
 }
 
@@ -273,56 +271,88 @@ for(let p of platforms){
 p.update();
 p.display();
 
+
+// collision with player
 if(
-
-velocityY>0 &&
+velocityY > 0 &&
 playerX > p.x &&
-playerX < p.x+p.w &&
-playerY+playerSize/2 > p.y &&
-playerY+playerSize/2 < p.y+p.h
-
+playerX < p.x + p.w &&
+playerY + playerSize/2 > p.y &&
+playerY + playerSize/2 < p.y + p.h
 ){
 
 velocityY = jumpForce;
 
 jumpSound.play();
 
-playerState="jump";
-currentAnimation=jumpFrames;
-currentFrame=0;
-
-if(p.type==="breakable") p.broken=true;
-
 }
 
 }
 
-for(let i=platforms.length-1;i>=0;i--){
-if(platforms[i].y>height+50){
-platforms.splice(i,1);
-}
-}
 
-// spawn platforms
-while(platforms.length<12){
+// spawn new platforms
+if(platforms.length < 12){
 
-let highestY = height;
+let highest = height;
 
 for(let p of platforms){
-if(p.y < highestY) highestY = p.y;
+if(p.y < highest) highest = p.y;
 }
 
-let newY = highestY - random(80,140);
-
-let type = random(["normal","normal","moving","breakable"]);
+let newY = highest - random(60,120);
 
 platforms.push(
-new Platform(random(width-120),newY,120,20,type)
+new Platform(
+random(width-120),
+newY,
+120,
+20
+)
 );
 
 }
 
+
+// remove platforms below screen
+for(let i=platforms.length-1;i>=0;i--){
+
+if(platforms[i].y > height + 50){
+platforms.splice(i,1);
 }
+
+}
+
+}
+// ======================================================
+// DAMAGE FUNCTION
+// ======================================================
+
+function damagePlayer(enemyX){
+
+if(invincible || gameOver) return;
+
+playerHealth -= 20;
+playerHealth = constrain(playerHealth,0,100);
+
+badFoodSound.play();
+
+playerState="hurt";
+currentAnimation=hurtFrames;
+currentFrame=0;
+
+if(playerX < enemyX) playerX -= knockbackX;
+else playerX += knockbackX;
+
+velocityY = knockbackY;
+
+invincible=true;
+invincibleTimer=millis();
+
+if(playerHealth<=0)
+triggerDeath();
+
+}
+
 
 // ======================================================
 // ENEMY CLASS
@@ -337,6 +367,7 @@ this.y=y;
 this.type=type;
 
 this.size=60;
+
 this.frame=0;
 
 this.dir=1;
@@ -350,7 +381,8 @@ if(this.type===1){
 
 this.x += this.speed*this.dir;
 
-if(this.x<30||this.x>width-30) this.dir*=-1;
+if(this.x<30||this.x>width-30)
+this.dir*=-1;
 
 }
 
@@ -361,9 +393,8 @@ this.y += this.speed;
 
 }
 
-if(frameCount%20===0){
-this.frame = 1-this.frame;
-}
+if(frameCount%20===0)
+this.frame=1-this.frame;
 
 }
 
@@ -371,9 +402,14 @@ display(){
 
 let img;
 
-if(this.type===1) img = enemy1[this.frame];
-if(this.type===2) img = enemy2[this.frame];
-if(this.type===3) img = enemy3[this.frame];
+if(this.type===1)
+img=this.frame===0?enemy1[0]:enemy1[1];
+
+if(this.type===2)
+img=this.frame===0?enemy2[0]:enemy2[1];
+
+if(this.type===3)
+img=this.frame===0?enemy3[0]:enemy3[1];
 
 image(img,this.x,this.y,this.size,this.size);
 
@@ -381,111 +417,94 @@ image(img,this.x,this.y,this.size,this.size);
 
 collide(){
 
-return dist(playerX,playerY,this.x,this.y) < this.size/2 + playerSize/2;
+return dist(playerX,playerY,this.x,this.y)
+< this.size/2 + playerSize/2;
 
 }
 
 }
+
 
 // ======================================================
-// ENEMIES
+// ENEMY SYSTEM
 // ======================================================
 
 function handleEnemies(){
 
-// safe enemy spawn
-if(score>1000 && frameCount%240===0){
-
-let p = random(platforms);
-
-let spawnX = p.x + p.w/2;
-let spawnY = p.y - 50;
-
-enemies.push(new Enemy(spawnX,spawnY,1));
-
-}
+if(score>1000 && frameCount%240===0)
+enemies.push(new Enemy(random(50,width-50),-50,1));
 
 if(score>2000 && frameCount%300===0){
 
-let p = random(platforms);
-
+let p=random(platforms);
 enemies.push(new Enemy(p.x+p.w/2,p.y-40,2));
 
 }
 
-if(score>4000 && frameCount%360===0){
+if(score>4000 && frameCount%360===0)
+enemies.push(new Enemy(random(width),-50,3));
 
-let p = random(platforms);
 
-enemies.push(new Enemy(p.x+p.w/2,p.y-60,3));
+for(let i=enemies.length-1;i>=0;i--){
 
-}
-
-for(let e of enemies){
+let e=enemies[i];
 
 e.update();
 e.display();
 
 if(e.collide()){
 
-playerHealth -= 30;
+// stomp enemy
+if(velocityY>0 && playerY < e.y){
 
-badFoodSound.play();
-
-playerState="hurt";
-currentAnimation=hurtFrames;
-currentFrame=0;
-
-if(playerHealth<=0){
-triggerDeath();
-}
-
-}
-
-}
-
-// cleanup
-for(let i=enemies.length-1;i>=0;i--){
-if(enemies[i].y > height+100){
+velocityY = jumpForce;
 enemies.splice(i,1);
-}
+
+score += 200;
+
+}else{
+
+damagePlayer(e.x);
+
 }
 
 }
+
+}
+
+}
+
 
 // ======================================================
-// FOOD
+// FRUITS
 // ======================================================
 
-function spawnFood(){
+function spawnFruits(){
 
 let types=["apple","banana","cherry","strawberry"];
 
-for(let i=0;i<4;i++){
+for(let i=0;i<2;i++){
 
-let type=random(types);
+let t = types[floor(random(types.length))];
 
-foods.push(
-new Food(type, random(50,width-50), random(-600,-50))
-);
+fruits.push(new Food(t));
 
 }
 
 }
 
-function handleFood(){
 
-for(let f of foods){
+function handleFruits(){
+
+for(let f of fruits){
 
 f.display();
 
-if(!f.active) continue;
+if(f.collide()){
 
-if(f.collides(playerX,playerY,playerSize)){
+if(f.type==="apple"){
 
-if(f.points < 0){
-
-playerHealth += f.points;
+playerHealth -= 20;
 
 badFoodSound.play();
 
@@ -495,18 +514,24 @@ currentFrame=0;
 
 }else{
 
-score += f.points;
+score += 100;
 goodFoodSound.play();
 
 }
 
-f.respawn();
+playerHealth = constrain(playerHealth,0,100);
+
+if(playerHealth<=0)
+triggerDeath();
+
+f.relocate();
 
 }
 
 }
 
 }
+
 
 // ======================================================
 // PLAYER ANIMATION
@@ -519,9 +544,13 @@ push();
 translate(playerX,playerY);
 scale(playerDirection,1);
 
+if(invincible && frameCount%6<3)
+tint(255,120);
+
 if(playerState==="idle"||velocityY>0){
 
 image(idleFrame,0,0,playerSize,playerSize);
+noTint();
 pop();
 return;
 
@@ -542,9 +571,11 @@ currentFrame=0;
 
 image(currentAnimation[currentFrame],0,0,playerSize,playerSize);
 
+noTint();
 pop();
 
 }
+
 
 // ======================================================
 // UI
@@ -567,13 +598,14 @@ rect(20,50,playerHealth*2,20);
 
 }
 
+
 // ======================================================
 // DEATH
 // ======================================================
 
 function triggerDeath(){
 
-gameState="gameover";
+gameOver=true;
 
 backgroundMusic.stop();
 deathSound.play();
@@ -584,6 +616,7 @@ currentFrame=0;
 
 }
 
+
 // ======================================================
 // GAME OVER
 // ======================================================
@@ -593,16 +626,16 @@ function showGameOver(){
 fill(0,200);
 rect(0,0,width,height);
 
-if(frameCount%animationSpeed===0){
-if(currentFrame<deathFrames.length-1) currentFrame++;
-}
+if(frameCount%animationSpeed===0)
+if(currentFrame<deathFrames.length-1)
+currentFrame++;
 
 image(deathFrames[currentFrame],width/2,height/2,150,150);
 
 fill(255);
 textAlign(CENTER);
-
 textSize(40);
+
 text("GAME OVER",width/2,120);
 
 textSize(20);
@@ -610,28 +643,17 @@ text("Press R to Restart",width/2,170);
 
 }
 
+
 // ======================================================
-// INPUT
+// CONTROLS
 // ======================================================
 
 function keyPressed(){
 
-if(gameState==="start" && key===" "){
-
+if(!gameStarted && key===" ")
 startGame();
-gameState="play";
 
-backgroundMusic.loop();
-
-}
-
-if(gameState==="gameover" && (key==="r"||key==="R")){
-
+if(gameOver && key==="r")
 startGame();
-gameState="play";
-
-backgroundMusic.loop();
-
-}
 
 }
